72c0e8924ab9989b966c7708f937576a1fbd1e8b
Malt <1072829851@qq.com>
2018-01-09 21:24:21 +0800

    用两个栈实现队列

---
+++ src/5.cpp
@@ -0,0 +1,39 @@
+// 分析：
+// push方法同栈，定义一个栈stack1，将元素压入即可
+// pop方法：定义另一个栈stack2。需要出队时，将stack1中的元素
+// 弹出压入stack2，并返回stack1弹出的栈底元素。后续出队，若
+// stack2中仍有元素，则直接弹出stack2顶部，否则将stack1弹出
+// 并压入stack2，入队直接压入stack1。
+
+class Solution
+{
+public:
+    void push(int node) {
+        stack1.push(node);
+    }
+
+    int pop() {
+		int stack1_size = stack1.size();
+		int stack2_size = stack2.size();
+		
+		if(stack2_size != 0)
+		{
+			int result = stack2.top();
+			stack2.pop();
+			return result;
+		}
+		for(int i = 0; i < stack1_size; i++)
+		{
+			int tmp = stack1.top();
+			stack2.push(tmp);
+			stack1.pop();
+		}
+		int result = stack2.top();
+		stack2.pop();
+		return result;
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
