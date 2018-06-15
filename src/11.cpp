b649591f11584d14e30c8e32e9578ffefda9b397
Malt <1072829851@qq.com>
2018-06-15 21:38:12 +0800

    二进制中1的个数

--- src/11.cpp
+++ src/11.cpp
@@ -6,6 +6,7 @@ public:
      int  NumberOf1(int n) {
          int number = 0;
          for(int i = 1; i <= 32; i++)  //若改写为while(n)，对于n为负，则会陷入死循环
+		 //负数首位为符号位，保持为1
          {
              if(n & 1 == 1)
                 number ++;
@@ -14,3 +15,20 @@ public:
          return number;
      }
 };
+
+//第二次
+//n & (n - 1)会将n的最右边的1置为0
+/*
+class Solution {
+public:
+     int  NumberOf1(int n) {
+		 int number = 0;
+		 while (n)
+		 {
+			 number++;
+			 n = n & (n - 1);
+		 }
+		 return number;
+     }
+};
+*/
