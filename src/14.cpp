fd486d2682c257d1f4da5e5223babb5f43e18fe8
Malt <1072829851@qq.com>
2018-06-18 20:42:53 +0800

    链表中倒数第k个结点

--- src/14.cpp
+++ src/14.cpp
@@ -40,3 +40,42 @@ public:
 		return second;
     }
 };
+
+/*
+//第二次
+/*
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+class Solution {
+public:
+    ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
+		if (pListHead == nullptr)
+			return nullptr;
+		ListNode *p = pListHead;
+		ListNode *q = pListHead;
+		while (k--)
+		{
+			p = p->next;
+			if (p == nullptr)
+			{
+				if (k == 0)	//如果刚好为倒数第k个，k为链表长度，则应返回头
+					break;
+				else	//如果k大于了链表长度
+					return nullptr;
+			}
+		}
+		while (p != nullptr)
+		{
+			p = p->next;
+			q = q->next;
+		}
+		return q;
+    }
+};
+*/
