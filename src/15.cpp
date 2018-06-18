d2ea04bc53838c07f4e1cde60969ef50614a02d4
Malt <1072829851@qq.com>
2018-06-18 20:57:19 +0800

    反转链表

--- src/15.cpp
+++ src/15.cpp
@@ -33,3 +33,36 @@ public:
 		return ReverseHead;
     }
 };
+
+//第二次
+// 定义一个pre指针和一个cur指针，在断掉cur->next指针时需要提前进行保存
+// 当cur_node->next为空时，pre_node指向前一个节点，cur_node指向最后一个
+// 节点，此时在退出循环后，直接将cur_node->next = pre_node即可。
+/*
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+class Solution {
+public:
+    ListNode* ReverseList(ListNode* pHead) {
+		if (pHead == nullptr)
+			return nullptr;
+		ListNode *pre_node = nullptr;
+		ListNode *cur_node = pHead;
+		while (cur_node->next)
+		{
+			ListNode *next_node = cur_node->next;
+			cur_node->next = pre_node;
+			pre_node = cur_node;
+			cur_node = next_node;
+		}
+		cur_node->next = pre_node;
+		return cur_node;
+    }
+};
+*/
